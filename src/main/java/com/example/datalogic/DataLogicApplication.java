0386a5c1d7561cc0b201d2cccd1ef5e6dc47fd25
Rahman <rahmanrejepov777@gmail.com>
2022-10-09 21:02:06 +0300

    updated configs

---
+++ src/main/java/com/example/datalogic/DataLogicApplication.java
@@ -0,0 +1,13 @@
+package com.example.datalogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLogicApplication.class, args);
+    }
+
+}
